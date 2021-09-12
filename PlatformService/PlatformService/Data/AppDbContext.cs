6cfd1e639241f4438c922d3fdeb39710f8f7bbb2
Vladislav Sheyko <vladislav.sheyko@hotmail.com>
2021-09-12 23:23:29 +0300

    Added Models and Context

---
+++ PlatformService/PlatformService/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+
+        private DbSet<Platform> Platforms { get; set; }
+        
+    }
+}
